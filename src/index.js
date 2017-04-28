3ee9c7fe96f397a522fb51f8e72e08e8c55a1dc8
Todd Briley <todd.briley@outlook.com>
2017-04-27 21:47:02 -0400

    uses ajax to contact the backend

--- src/index.js
+++ src/index.js
@@ -2,53 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TransitionGroup from 'react-transition-group/CSSTransitionGroup';
-
-let contacts =[{name:'Tim Adams',phone:'404-555-1212', email: 'tim.adams@gmail.com',type:'Friend'},{name:'Barney Rubble',phone:'14', email: 'barney_rub@rubble.com',type:'Neighbor'},
-              {name:'Great Gazoo',phone:'17', email: 'the-great-one@gmail.com',type:'Alien'}];
+const $ = window.jQuery;
 
 class ContactList extends React.Component {
   constructor(){
     super();
     this.state = {
-      name: '',
-      phone: '',
-      email: '',
-      type: '',
-      contacts: contacts,
+      name: ' ',
+      phone: ' ',
+      email: ' ',
+      type: 'Friend',
+      contacts: [],
       mode: 'input',
-      contactIndex: ''
+      contactIndex: '',
+      favorite: 'FALSE'
     }
   }
 
-  submit(event){
+  componentDidMount(){
+    $.get('http://localhost:5004/api/contacts')
+    .then(contacts => {
+      this.setState({contacts:contacts});
+    })
+    .catch(err => console.log(err.message));
+  }
+
+  addContact(event){
     event.preventDefault();
     if (this.state.name !== ''){
       console.log('submiting new contact');
-      this.state.contacts.push({
-        name: this.state.name,
-        phone: this.state.phone,
-        email: this.state.email,
-        type: this.state.type
-      });
-      this.setState({
-        name: '',
-        phone: '',
-        email: '',
-        type: '',
-        contacts: this.state.contacts
+      let newContact = {name: this.state.name, phone: this.state.phone,
+        email: this.state.email, type:this.state.type};
+      $.ajax({
+        method: 'POST',
+        url: 'http://localhost:5004/api/contacts',
+        data: JSON.stringify(newContact),
+        contentType: 'application/json'
       })
-    }
+      .then((contact) => {
+        this.setState(contact);
+        return contact;
+      } )
+      .then(contact => this.state.contacts.push(contact))
+      .then((val) => {
+        this.setState({
+          name: '',
+          phone: '',
+          email: '',
+          type: '',
+          contacts: this.state.contacts
+        });
+      })
+      .catch(err => console.log(err.message));
 
-  class Contact extends React.Component {
-    constructor(){
-      super();
-
     }
-  }
 
   }
   sumthangChanged(item,event){
-    console.log('changing: ' + item + ' to: ' + event.target.value);
     this.setState({
       [item]:event.target.value
     })
@@ -56,43 +66,51 @@ class ContactList extends React.Component {
   }
 
   deleteContact(idx){
-    if (idx !== this.state.contactIndex) {
-      let contactstmp =  this.state.contacts;
-      contactstmp.splice(idx,1);
-      this.setState({
-        contacts: contactstmp
-      });
-    } else {
-      let contactstmp =  this.state.contacts;
-      contactstmp.splice(idx,1);
-      this.setState({
-        contacts: contactstmp,
-        name: '',
-        phone: '',
-        email: '',
-        mode: 'input',
-        contactIndex: ''
-      });
-    }
+    console.log("deleting contact with db id of: " + idx);
+    $.ajax({
+      method: 'DELETE',
+      url: 'http://localhost:5004/api/contacts/'+idx
+    })
+    .then( value => console.log("delete api return value: " + value))
+    .then( value => {
+      let contactstmp = this.state.contacts;
+      contactstmp = contactstmp.filter(item => item.id !== idx);
+      this.setState({contacts: contactstmp, type:'friend', name: '',phone:'',email:'',mode:'input'});
+      }
+    )
+    .catch( err => console.log("error: " + err.message));
 
+    this.setState({name: '',phone:'',email:'',mode:'input'});
   }
 
   saveEdit(){
     console.log("saving Edit");
-    let contactstmp = this.state.contacts;
-    let idx = this.state.contactIndex;
-    contactstmp[idx]['name']=this.state.name;
-    contactstmp[idx]['phone']=this.state.phone;
-    contactstmp[idx]['email']=this.state.email;
-    contactstmp[idx]['type']=this.state.type;
-    this.setState({
-      contacts: contactstmp,
-      name: '',
-      phone: '',
-      email: '',
-      mode: 'input'
+    // let contactstmp = this.state.contacts;
+    let idx = this.state.id;
+    console.log("contact id in Database: " + idx);
+    let contactObject = {name: this.state.name,
+      phone: this.state.phone,
+      email: this.state.email,
+      type: this.state.type,
+      favorite: this.state.favorite,
+      id: this.state.id
+    };
+    console.log('contactObject is on next line');
+    console.log(contactObject);
+    $.ajax({
+      method: 'PUT',
+      url: 'http://localhost:5004/api/contacts/'+idx,
+      data: JSON.stringify(contactObject),
+      contentType: 'application/json'
     })
+    .then( contact => {console.log('contact returned: ' + contact); return contact;})
+    .then( contact => {
+      let contactstmp = this.state.contacts;
+      contactstmp = contactstmp.map(item => item.id === idx ? contact : item );
+      console.log(contactstmp);
+      this.setState({contacts: contactstmp, name: '', phone: '', email: '', type: 'friend', favorite: '', mode:'input' });
 
+    });
   }
 
   cancelEdit(){
@@ -109,58 +127,69 @@ class ContactList extends React.Component {
 
   editContact(idx){
     console.log("in edit");
-    let contacttmp =  this.state.contacts[idx];
-    this.setState({
-      name: contacttmp['name'],
-      phone: contacttmp['phone'],
-      email: contacttmp['email'],
-      type: contacttmp['type'],
-      mode: 'edit',
-      contactIndex: idx
-    })
+    console.log("looking for contact with DB id of: " + idx);
+    let contacttmp =  this.state.contacts.filter(contact => contact.id === idx);
+    contacttmp = contacttmp[0];
+    contacttmp.mode = 'edit';
+    console.log(contacttmp.id);
+    console.log(contacttmp);
+    this.setState(contacttmp);
+
   }
   render(){
 
     return (
       <div className="outerContainer">
-        <div className="formContainer">
-          <form className="theForm form-group" onSubmit={(event)=>this.submit(event)} method="post">
-            <label for="name">Name</label>
-            <input className="form-control" type="text" onChange={(event) => this.sumthangChanged('name',event)} name="name" value={this.state.name}/>
-            <label for="phone">Phone</label>
-            <input className="form-control" type="text" onChange={(event) => this.sumthangChanged('phone',event)} name="phone" value={this.state.phone}/>
-            <label for="email">Email</label>
-            <input className="form-control"  type="text" onChange={(event) => this.sumthangChanged('email',event)} name="email" value={this.state.email}/>
-            <label for="type">Type</label>
-            <select className="form-control" onChange={(event) => this.sumthangChanged('type',event)} name="type" value={this.state.type}>
-              <option value="Friend">Friend</option>
-              <option value="Co-worker">Co-worker</option>
-              <option value="Daughter">Daughter</option>
-              <option value="Son">Son</option>
-              <option value="Neighbor">Neighbor</option>
-              <option value="Parent">Parent</option>
-              <option value="Alien">Alien</option>
-            </select>
-            {this.state.mode ==='input' ? <button className="addButton" type="submit">Submit</button>: null}
-            {this.state.mode ==='edit' ? <button onClick={()=>this.saveEdit()} className="saveEditButton" type="button">Save</button>:null}
-            {this.state.mode ==='edit' ? <button onClick={()=>this.cancelEdit()} className="cancelEditButton" type="button">Cancel</button>: null}
-          </form>
+        <div className="add"><button data-toggle="modal" data-target="#inputForm" className="addEntry" onClick={()=>this.addEntry()}>Add</button></div>
+      <div id="inputForm" className="modal fade in" role="dialog">
+        <div className="modal-dialog">
+
+          <div className="formContainer modal-body">
+            <form className="theForm form-group modal-content" onSubmit={(event)=>this.addContact(event)} method="post">
+              <label htmlFor="name">Name</label>
+              <input className="form-control" type="text" onChange={(event) => this.sumthangChanged('name',event)} name="name" value={this.state.name}/>
+              <label htmlFor="phone">Phone</label>
+              <input className="form-control" type="text" onChange={(event) => this.sumthangChanged('phone',event)} name="phone" value={this.state.phone}/>
+              <label htmlFor="email">Email</label>
+              <input className="form-control"  type="text" onChange={(event) => this.sumthangChanged('email',event)} name="email" value={this.state.email}/>
+              <label htmlFor="type">Type</label>
+              <select className="form-control" onChange={(event) => this.sumthangChanged('type',event)} name="type" value={this.state.type}>
+                <option value="Friend">Friend</option>
+                <option value="Co-worker">Co-worker</option>
+                <option value="Daughter">Daughter</option>
+                <option value="Son">Son</option>
+                <option value="Neighbor">Neighbor</option>
+                <option value="Parent">Parent</option>
+                <option value="Alien">Alien</option>
+              </select>
+              {this.state.mode ==='input' ? <button className="addButton" type="submit">Submit</button>: null}
+              {this.state.mode ==='edit' ? <button data-dismiss="modal" onClick={()=>this.saveEdit()} className="saveEditButton" type="button">Save</button>:null}
+              {this.state.mode ==='edit' ? <button data-dismiss="modal" onClick={()=>this.cancelEdit()} className="cancelEditButton" type="button">Cancel</button>: null}
+            </form>
+          </div>
+        </div>
         </div>
         <div>Contacts</div>
         <div className="contacts">
-            {this.state.contacts.map((contact,idx) => <div className="contactBox">
-              <div className='edit'>
-                <img className="editIcon" onClick={()=>this.editContact(idx)} src="/images/Edit.png"/>
+          <TransitionGroup transitionName="kill" transitionLeaveTimeout={300} transitionEnterTimeout={300}>
+            {this.state.contacts.map((contact,idx) =>
+
+              <div key={contact['id']} data-id={contact['id']} className="contactBox">
+                <div className='edit'>
+                  <img alt="editicon" className="editIcon" data-toggle="modal" data-target="#inputForm" onClick={()=>this.editContact(contact['id'])} src="/images/Edit.png"/>
+                </div>
+                <div className="details">
+                  <ul>
+                    <li> {contact['name']} - {contact['type']}</li>
+                    <li> {contact['phone']}</li>
+                    <li> {contact['email']}</li>
+
+                  </ul>
+                </div>
+                <div onClick={() => this.deleteContact(contact['id'])} className="deleteButton">X</div>
               </div>
-              <div className="details">
-                <ul>
-                  <li>{contact['name']} - {contact['type']}</li>
-                  <li>{contact['phone']}</li>
-                  <li>{contact['email']}</li>
-                </ul>
-              </div>
-              <div onClick={() => this.deleteContact(idx)} className="deleteButton">X</div>
-            </div>)}
+            )}
+          </TransitionGroup>
 
         </div>
       </div>
@@ -170,6 +199,6 @@ class ContactList extends React.Component {
 }
 
 ReactDOM.render(
-  <ContactList contacts={contacts} />,
+  <ContactList/>,
   document.getElementById('root')
 );
